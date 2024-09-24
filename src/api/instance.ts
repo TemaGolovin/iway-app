6c85571f73e7c353ace90d187e8a469c660b515d
a.golovin <a.golovin@iq-adv.ru>
2024-09-24 20:18:55 +0500

    main | feat: add auth header each request

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,5 +1,23 @@
-import axios from "axios";
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+
+const reqInterceptor = async (request: InternalAxiosRequestConfig) => {
+  const accessToken = localStorage.getItem("token");
+
+  if (accessToken) {
+    request.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return request;
+};
+
+const errReqInterceptor = (error: AxiosError) => Promise.reject(error);
+
+
+axiosInstance.interceptors.request.use(reqInterceptor, errReqInterceptor);
+
+export { axiosInstance };
